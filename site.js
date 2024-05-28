db9419960c2508db722cb83a561a5c560492eba4
jaswika kilaru <159292089+JaswikaKilaru@users.noreply.github.com>
2024-05-28 15:52:37 +0530

    Add files via upload

---
+++ site.js
@@ -0,0 +1,45 @@
+const executiveMembers = [
+    {
+      firstname: "SAI KUMAR",
+      lastname: "ADIREDDY",
+      role: "EXECUTIVE Member",
+      imageUrl: "/teams/present/SAIKUMAR_ADIREDDY.jpg",
+      details: "3rd year, ME",
+    },
+
+    {
+        firstname: "KANIKICHARLA",
+        lastname: "BALA GANESH",
+        role: "EXECUTIVE Member",
+        imageUrl: "/teams/present/KANIKICHARLA_BALAGANESH.jpg",
+        details: "3rd year, CSE",
+      },
+
+
+      {
+        firstname: "PAPPU",
+        lastname: "SHARMILA",
+        role: "EXECUTIVE Member",
+        imageUrl: "/teams/present/PAPPU_SHARMILA.jpg",
+        details: "3rd year, ECE",
+      },
+
+      {
+        firstname: "R S B Bikarna",
+        lastname: "Baidya",
+        role: "EXECUTIVE Member",
+        imageUrl: "/teams/present/R S B Bikarna_Baidya.jpg",
+        details: "3rd year, CE",
+      },
+      
+      {
+        firstname: "Supriyo",
+        lastname: "Mondal",
+        role: "EXECUTIVE Member",
+        imageUrl: "/teams/present/Supriyo_Mondal.jpg",
+        details: "3rd year, CE",
+      },
+  
+    
+  ];
+  
