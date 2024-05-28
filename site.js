0cf174b043de29fe53218598dd76c5045f44dc5c
Safiya2358 <130473668+Safiya2358@users.noreply.github.com>
2024-05-28 17:06:44 +0530

    Update site.js
    Added Details

--- site.js
+++ site.js
@@ -1,45 +1,139 @@
 const executiveMembers = [
     {
-      firstname: "SAI KUMAR",
+      firstname: "SAI KUMAR ",
       lastname: "ADIREDDY",
-      role: "EXECUTIVE Member",
+      role: "SAIC Executive Member",
       imageUrl: "/teams/present/SAIKUMAR_ADIREDDY.jpg",
       details: "3rd year, ME",
     },
 
     {
-        firstname: "KANIKICHARLA",
+        firstname: "KANIKICHARLA ",
         lastname: "BALA GANESH",
-        role: "EXECUTIVE Member",
+        role: "SAIC Executive Member",
         imageUrl: "/teams/present/KANIKICHARLA_BALAGANESH.jpg",
         details: "3rd year, CSE",
       },
 
 
       {
-        firstname: "PAPPU",
+        firstname: "PAPPU ",
         lastname: "SHARMILA",
-        role: "EXECUTIVE Member",
+        role: "SAIC Executive Member",
         imageUrl: "/teams/present/PAPPU_SHARMILA.jpg",
         details: "3rd year, ECE",
       },
 
       {
-        firstname: "R S B Bikarna",
+        firstname: "R S B Bikarna ",
         lastname: "Baidya",
-        role: "EXECUTIVE Member",
+        role: "SAIC Executive Member",
         imageUrl: "/teams/present/R S B Bikarna_Baidya.jpg",
         details: "3rd year, CE",
       },
       
       {
-        firstname: "Supriyo",
+        firstname: "Supriyo ",
         lastname: "Mondal",
-        role: "EXECUTIVE Member",
+        role: "SAIC Executive Member",
         imageUrl: "/teams/present/Supriyo_Mondal.jpg",
         details: "3rd year, CE",
       },
-  
     
+      {
+       firstname: "Vishal ",
+       lastname: "Gupta",
+       role: "SAIC Executive Member",
+       imageUrl: "/teams/present/Vishal_Gupta.jpg",
+       details: "3rd year, ME",
+      },
+    
+      {
+       firstname: "Sanjib ",
+       lastname: "Maity",
+       role: "SAIC Executive Member",
+       imageUrl: "/teams/present/SANJIB_MAITY.jpg",
+       details: "3rd year, MME",
+      },
+    
+      {
+       firstname: "Swarnim ",
+       lastname: "Suryansh",
+       role: "SAIC Executive Member",
+       imageUrl: "/teams/present/Swarnim_Suryansh.jpg",
+       details: "3rd year, MME",
+      },
+     {
+      firstname: "Rahul ",
+      lastname: "Mondal",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Rahul_Mondal.jpg",
+      details: "3rd year, ME",
+     },
+    
+     {
+      firstname: "Soumili ",
+      lastname: "Ghosh",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Soumili_Ghosh.jpg",
+      details: "3rd year, MME",
+     },
+
+     {
+      firstname: "Katravath ",
+      lastname: "Navya",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Katravath_Navya.jpg",
+      details: "3rd year, ME",
+     },
+    
+     {
+      firstname: "Pratishtha ",
+      lastname: "Barua",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Pratishtha_Barua.jpg",
+      details: "3rd year, EE",
+     },
+
+     {
+      firstname: "Soumi ",
+      lastname: "Ghosh",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Soumi_Ghosh.jpg",
+      details: "3rd year, CH",
+     },
+
+     {
+      firstname: "Souhardya ",
+      lastname: "Majumder",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Souhardya_Majumder.jpg",
+      details: "3rd year, ECE",
+     },
+
+     {
+      firstname: "Uppada ",
+      lastname: "Sravanthi",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Uppada_Sravanthi.jpg",
+      details: "3rd year, ME",
+     },
+
+    {
+      firstname: "Reshmi ",
+      lastname: "Saha",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Reshmi_Saha.jpg",
+      details: "3rd year, ECE",
+    },
+    {
+      firstname: "Deepjyoti ",
+      lastname: "Dutta",
+      role: "SAIC Executive Member",
+      imageUrl: "/teams/present/Deepjyoti_Dutta.jpg",
+      details: "3rd year, CE",
+     },
+    
+       
   ];
   
